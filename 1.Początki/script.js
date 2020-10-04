b76180d0b0bfaaabc0694a006098f0e9c0bb2b9c
neacatsu <wiktoria.wierzbicka166@gmail.com>
2020-10-04 17:49:52 +0200

    add folder 1

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//1:
+console.log("Wiktoria");
+
+// 2
+console.log(12+32);
+console.log(52/16); // zmiennoprzecinkowa
+console.log(5*2);
+
+// 3
+console.log("Hello World");
+//console.log(Hello World); // bład, nieznana zmienna
+// console.log("Hello World); //bład, niezakończony string
