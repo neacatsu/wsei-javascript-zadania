51abec0699a197b7a85eff3f76f2079f8ab1c20b
neacatsu <wiktoria.wierzbicka166@gmail.com>
2020-10-04 18:56:46 +0200

    Add folder 3

---
+++ 3.Operatory/script.js
@@ -0,0 +1,38 @@
+//1
+let a = true;
+let b = false;
+console.log(a==b);
+
+//2
+let x = 5;
+let y = 10;
+let moduloResult = 0;
+moduloResult = x%y;
+console.log(moduloResult);
+
+//3
+let string1 = "Lorem";
+let string2 = "ipsum";
+
+let stringResult = string1 + " " + string2;
+console.log(stringResult);
+
+//4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString); // true ponieważ zmienne mają zgodne typy
+console.log(someNumber === someString); // false ponieważ zmienne nie są identyczne
+
+//5
+
+let counter = 30;
+console.log(++counter);
+console.log(--counter);
+
+//6
+let num1 = 10;
+let num2 = 12;
+let result = null;
+result = num1>num2;
+console.log(result);
