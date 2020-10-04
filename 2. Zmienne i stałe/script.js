d5a6530896025a88bf240d549fd19d725d6dbf11
neacatsu <wiktoria.wierzbicka166@gmail.com>
2020-10-04 18:09:21 +0200

    add Folder 2

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,26 @@
+// 1
+/*let a = 6;
+let imie = "Wiktoria";
+let b = 2 + "dwa";
+let flag = true ;
+let c = null;
+
+console.log(a);
+console.log(imie);
+console.log(b);
+console.log(flag);
+console.log(c);
+*/
+
+// 2
+/*const a = 2;
+const b = 6;
+let sum = 0;
+sum = a+b;
+console.log(sum);
+*/
+
+// 3
+let d;
+console.log(d);
+//występuje undefinded ponieważ nic nie jest przypisane do zmiennej 
